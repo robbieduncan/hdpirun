fcb446e33c75960e3db85f718b77e4bb8db996d0
Robbie Duncan <robbieduncan@me.com>
2016-09-10 22:21:42 +0100

    Add wrapper to stop reinclude of library headers

---
+++ include/libhdhomerun.h
@@ -0,0 +1,10 @@
+// Wrapper to prevent multiple imports
+#ifndef LIBHDHOMERUN_H
+#define LIBHDHOMERUN_H
+
+#import "hdhomerun_os.h"
+#import "hdhomerun_discover.h"
+#import "hdhomerun_pkt.h"
+
+#endif
+ 
