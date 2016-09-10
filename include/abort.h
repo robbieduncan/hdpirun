cc5defc63138961a30f4712370771f9df5260382
Robbie Duncan <robbieduncan@me.com>
2016-09-10 07:43:28 +0100

    Close the log file not abort. Add standard header comments

--- include/abort.h
+++ include/abort.h
@@ -1,7 +1,16 @@
+# abort.h
+# hdpirun
+#
+# Utility header to abort the program on an unrecoverable error. Prints the message (printf format) and closes any open resources
+#
+# Author: Robbie Duncan
+# Copyright: Copyright (c) 2016 Robbie Duncan
+# License: See LICENSE
+#
 #import "log.h"
 #import <stdio.h>
 
 #ifndef ABORT_H
 #define ABORT_H
-#define __ABORT(message,...) __LOG(__LOG_CRITICAL,message,##__VA_ARGS__);if (__logFile!=stdout){fclose(stdout);};return 1;
+#define __ABORT(message,...) __LOG(__LOG_CRITICAL,message,##__VA_ARGS__);if (__logFile!=stdout){fclose(__logFile);};exit(1);
 #endif
