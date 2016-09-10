e109a7d3a76daf979ebfff93111254506ced547d
Robbie Duncan <robbieduncan@me.com>
2016-09-10 22:10:58 +0100

    Fix style

--- src/log.c
+++ src/log.c
@@ -1,18 +1,13 @@
 #include "log.h"
 
-int __LOG_TRACE = 0;
-int __LOG_DEBUG = 1;
-int __LOG_MESSAGE = 2;
-int __LOG_WARN = 3;
-int __LOG_ERROR = 4;
-int __LOG_CRITICAL = 5;
 char __LOG_NAMES[6][9] = {"TRACE","DEBUG","MESSAGE","WARN","ERROR","CRITICAL"};
 
-int __logLevel = 2;
-FILE* __logFile;
-
+FILE *logFile;
+// "Shared" variable that controls logging level
+LogLevel logLevel;
 
 void __logInit()
 {
-	__logFile = stdout;
+	logLevel = message;
+	logFile = stdout;
 }
