ac24377fd590cec5723eb588eeb65bd5a47e997c
Robbie Duncan <robbieduncan@me.com>
2016-09-11 08:49:01 +0100

    Move discovery reply packet construction to the correct file

--- include/homerun.h
+++ include/homerun.h
@@ -1,13 +1,22 @@
-// homerun.h
-// HDHomeRun Wrapper
-
+// network.h
+// hdpirun
+//
+// HDHomeRun wrapper. Wraps library functionality for use
+//
+// Author: Robbie Duncan
+// Copyright: Copyright (c) 2016 Robbie Duncan
+// License: See LICENSE
+//
 #include <stdint.h>
 #include "libhdhomerun.h"
-
+//
 #ifndef HOMERUN_H
 #define HOMERUN_H
 
 int verifyHomerunTuner(int);
+//
+// Constructs a packet to use to respond to a discovery request. The caller is responsible for destroying the packet
+struct hdhomerun_pkt_t* getDiscoveryReplyPacket();
 struct hdhomerun_discover_device_t getValidDevice();
 	
 #endif
