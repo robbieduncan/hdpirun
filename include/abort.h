b6bc7198dc356abbdec2bf326a7a5a075b5e8421
Robbie Duncan <robbieduncan@me.com>
2016-09-11 08:57:42 +0100

    Fix comment style again

--- include/abort.h
+++ include/abort.h
@@ -1,16 +1,19 @@
-// abort.h
-// hdpirun
-//
-// Utility header to abort the program on an unrecoverable error. Prints the message (printf format) and closes any open resources
-//
-// Author: Robbie Duncan
-// Copyright: Copyright (c) 2016 Robbie Duncan
-// License: See LICENSE
-//
+/* 
+   abort.h
+   hdpirun
+  
+   Utility header to abort the program on an unrecoverable error. Prints the message (printf format) and closes any open resources
+  
+   Author: Robbie Duncan
+   Copyright: Copyright (c) 2016 Robbie Duncan
+   License: See LICENSE
+*/
 #import <stdio.h>
 #import "log.h"
 
 #ifndef ABORT_H
 #define ABORT_H
+//
+// ABORT macro. Uses the LOG macro to display the message, does any cleanup the exits with failuur
 #define ABORT(message,...) LOG(critical,message,##__VA_ARGS__);if (logFile!=stdout){fclose(logFile);};exit(1);
 #endif
