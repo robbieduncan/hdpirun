1b8258f04aae897b17ba7902f1f27af50a694390
Robbie Duncan <robbieduncan@me.com>
2016-09-11 11:24:07 +0100

    Fix comment style again

--- include/homerun.h
+++ include/homerun.h
@@ -1,22 +1,26 @@
-// network.h
-// hdpirun
-//
-// HDHomeRun wrapper. Wraps library functionality for use
-//
-// Author: Robbie Duncan
-// Copyright: Copyright (c) 2016 Robbie Duncan
-// License: See LICENSE
-//
+/*
+   homerrun.h
+   hdpirun
+  
+   HDHomeRun wrapper. Wraps library functionality for use
+  
+   Author: Robbie Duncan
+   Copyright: Copyright (c) 2016 Robbie Duncan
+   License: See LICENSE
+*/
 #include <stdint.h>
 #include "libhdhomerun.h"
 //
 #ifndef HOMERUN_H
 #define HOMERUN_H
-
+//
+// Called to verify there is a tuner on the network. Int is the IP address of the tuner expected. This is optional: pass -1 if any tuner is acceptable
 int verifyHomerunTuner(int);
 //
 // Constructs a packet to use to respond to a discovery request. The caller is responsible for destroying the packet
 struct hdhomerun_pkt_t* getDiscoveryReplyPacket();
+//
+// Returns the device validated by verifyHomerunTuner
 struct hdhomerun_discover_device_t getValidDevice();
-	
+//
 #endif
