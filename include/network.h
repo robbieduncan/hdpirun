4b951db0f72f25b5e70e6df78ccdfbf83eae5bbc
Robbie Duncan <robbieduncan@me.com>
2016-09-11 08:45:48 +0100

    Add standard header comment

--- include/network.h
+++ include/network.h
@@ -1,6 +1,12 @@
 // network.h
-// network IO
-
+// hdpirun
+//
+// Network communication
+//
+// Author: Robbie Duncan
+// Copyright: Copyright (c) 2016 Robbie Duncan
+// License: See LICENSE
+//
 #ifndef NET_H
 #define NET_H
 
